68f91bae87a3dc8b0c23fc2d319f2abd793c8324
Inkim1 <matthew.inkim@mail.utoronto.ca>
2023-09-11 18:25:10 -0400

    Created multiples

---
+++ src/Multiples.java
@@ -0,0 +1,26 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int x = 0;
+        int i = 1;
+        while (i < 10) {
+
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3) {
+
+                x += 1;
+
+            } else if (divisibleBy5) {
+
+                x += 1;
+
+            }
+            i++;
+        }
+
+        System.out.println(x);
+    }
+}
